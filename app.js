5b79129dc7c36674e7d208eb06f3ef1012adab82
kaenguru123 <daskaenguru1234@gmail.com>
2022-12-15 20:25:55 +0100

    beginning of frontend

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const fetch = require("node-fetch");
+require('dotenv').config();
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("puplic"));
+
+app.use(express.urlencoded({
+    extented : true
+}))
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/", (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
