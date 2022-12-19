cc72148b8fa2a8674879a6bac388b4d78eec5f69
kaenguru123 <daskaenguru1234@gmail.com>
2022-12-20 00:20:34 +0100

    current status of file reading: problem with reading File async and with downloading from link

--- app.js
+++ app.js
@@ -2,7 +2,60 @@ const { response } = require("express");
 const express = require("express");
 const fetch = require("node-fetch");
 require('dotenv').config();
-const { readFile } = require("fs/promises");
+const axios = require("axios");
+const fs = require('fs');
+
+function makeToList(text){
+    return 
+}
+
+function getYtId(input){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = input.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false
+}
+
+function getAxiosOption(vIDOrUrl, onlyId = true){
+    console.log("inside option generation")
+    if (onlyId) {
+        return {
+            method: 'GET',
+            url: `https://youtube-mp36.p.rapidapi.com/dl?id=${vIDOrUrl}`,
+            headers: {
+                'X-RapidAPI-Key': process.env.API_KEY,
+                'X-RapidAPI-Host': process.env.API_HOST
+            }};
+        }
+    else {
+        return {
+            method: 'GET',
+            url: `${vIDOrUrl}`,
+            // headers: {
+            //     'X-RapidAPI-Key': process.env.API_KEY,
+            //     'X-RapidAPI-Host': process.env.API_HOST
+            // }
+        };
+    } 
+}
+
+function makeAxiosReqest(res, options){
+    axios.request(options).then(function (response) {
+        return res.render("index", {success : true, song_title: response.data.title, 
+            song_link : response.data.link});
+        }).catch(function (error) {
+            return res.render("index", {success : false, message : error});
+        });
+}
+
+function makeAxiosReqestAndDownload(res, options){
+    axios.request(options).then(function (response) {
+        options = getAxiosOption(response.data.link, onlyId = false);
+        console.log("make download request" + response.data.link)
+        axios.request(options);
+        }).catch(function (error) {
+            return res.render("index", {success : false, message : error});
+        });
+}
 
 const app = express()
 
@@ -25,19 +78,35 @@ app.post("/upload-file", (req, res) => {
     path = req.body.filePath.replace(/\\/g, "/");
     console.log(path);
 
-    const fileContent = readFile(path, 'utf-8', (error, data) => {
-        console.log(data);
-    });
-    
-    const url_list = null;
+    let content = null;
 
+    (() => {
+        const data = fs.promises.readFile(path, 'utf8');
+        console.log(data)
+        content = data.body.split('\r\n');;
+
+        for (let i = 0; i < content.length; ++i){
+            const url = content[i];
+            console.log(url + `loop ${i}`)
+            const ID = getYtId(url);
+            const options = getAxiosOption(ID);
+            console.log("make api request")
+            makeAxiosReqestAndDownload(res, options);
+        }
+        // content.forEach(url => {
+        //     console.log(url + `loop ${cnt}`)
+        //     cnt = cnt + 1
+        //     const ID = getYtId(url);
+        //     const options = getAxiosOption(ID);
+        //     console.log("make api request")
+        //     makeAxiosReqestAndDownload(res, options);
+        // });
+      })();
     return res.render("index", );
 })
 
 app.post("/convert-mp3", async (req, res) => {
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = req.body.videoID.match(regExp);
-    const videoID = (match&&match[7].length==11)? match[7] : false
+    const videoID = getYtId(req.body.videoID)
     
     if (videoID === "undefined" ||
         videoID === "" ||
@@ -47,26 +116,8 @@ app.post("/convert-mp3", async (req, res) => {
     }
     else
     {   
-        console.log("Started convert-func")
-        const axios = require("axios");
-        
-
-        const options = {
-        method: 'GET',
-        url: `https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}`,
-        headers: {
-            'X-RapidAPI-Key': process.env.API_KEY,
-            'X-RapidAPI-Host': process.env.API_HOST
-        }
-        };
-        console.log(options);
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-            return res.render("index", {success : true, song_title: response.data.title, 
-                song_link : response.data.link});
-        }).catch(function (error) {
-            return res.render("index", {success : false, message : error});
-        });
+        const options = getAxiosOption(videoID)
+        return makeAxiosReqest(res, options)
     }
 })
 
